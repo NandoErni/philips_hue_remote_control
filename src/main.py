1b73c8c4348152e5ad09622da81ff5cc98c16f5a
Nando Erni <nando.erni@outlook.de>
2021-01-03 15:38:11 +0100

    add gpio functionality

--- src/main.py
+++ src/main.py
@@ -1,5 +1,22 @@
-# to make this work install python3, pip3, requests
+# to make this work install python3, pip3, requests, RPi.GPIO for python3
 import apiRepository
+import RPi.GPIO as GPIO
+import time
+
+button = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+state = False
+flagging = False
+while True:
+    buttonPress = GPIO.input(button)
+
+    if buttonPress and not flagging:
+        state = not state
+
+    flagging = buttonPress
 
 print(apiRepository.isHueAvailable())
 print('done!')
