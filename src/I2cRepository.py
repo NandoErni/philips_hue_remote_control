e25c3ed946dc12250100d7ec71745ebedd09b4fd
Nando Erni <nando.erni@outlook.de>
2021-01-31 18:32:19 +0100

    trying out stuff

--- src/I2cRepository.py
+++ src/I2cRepository.py
@@ -30,7 +30,7 @@ class I2cRepository:
 
         self.clear()
         self.draw.text(
-            (self.oled.width / 2 - font_width / 2, -5),
+            (self.oled.width / 2 - font_width / 2, -15),
             text,
             font=self.font,
             fill=255,
