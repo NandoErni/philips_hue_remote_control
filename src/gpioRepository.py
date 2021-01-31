c521f9de67995fb163ca877d3cc314dca9c19420
Nando Erni <nando.erni@outlook.de>
2021-01-31 12:22:51 +0100

    just trying out values

--- src/gpioRepository.py
+++ src/gpioRepository.py
@@ -61,7 +61,7 @@ class GPIORepository:
             if not self.buttonFlags[gpio_button_flag_index]:
                 self.buttonFlags[gpio_button_flag_index] = True
                 print(outputString)
-                time.sleep(0.005)
+                time.sleep(0.01)
                 return True
             else:
                 return False
