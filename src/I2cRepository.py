623d3d06566be238e6c06cd222f88f7d32498c2e
Nando Erni <nando.erni@outlook.de>
2021-01-31 18:30:03 +0100

    fix oled

--- src/I2cRepository.py
+++ src/I2cRepository.py
@@ -23,7 +23,7 @@ class I2cRepository:
         self.font = ImageFont.truetype(config.STANDARD_FONT, config.STANDARD_FONT_SIZE)
 
     def clear(self):
-        self.draw.rectangle((100, 100, 300, 150), fill=0)
+        self.draw.rectangle((0, 0, 300, 150), fill=0)
 
     def writeText(self, text):
         (font_width, font_height) = self.font.getsize(text)
