e2f3b37591e121e75b952d539a8da503af5e9aa3
Nando Erni <nando.erni@outlook.de>
2021-01-31 02:26:09 +0100

    fix

--- src/apiRepository.py
+++ src/apiRepository.py
@@ -22,7 +22,7 @@ class APIRepository:
 
     def toggleLight(self, light):
         r = self.httpGet(self.getLightEndpoint(light))
-        state = not r.json()["state"]["on"]
+        state = not r.json()[0]["on"]
         return self.setOnStateToLight(light, state)
 
     def setBrightnessToGroup(self, group, brightness):
