b5cb2de5dad8e07f5362e2326d9092de0f0b61e8
Nando Erni <nando.erni@outlook.de>
2021-02-02 02:04:18 +0100

    fix waitForConnect

--- src/waitForConnection.py
+++ src/waitForConnection.py
@@ -2,19 +2,23 @@ import config
 import i2cRepository
 import requests
 import subprocess
-import time
 
 display = i2cRepository.I2cRepository()
 
 display.initDisplay()
 display.writeText("Starting...")
 
-r = requests.get(config.SAMPLE_URL, timeout=5)
-while not r.status_code == 200:
-    try:
-        r = requests.get(config.SAMPLE_URL, timeout=5)
-    except:
-        pass
 
+def waitUntilSuccess():
+    while True:
+        try:
+            r = requests.get(config.SAMPLE_URL, timeout=5)
+            if r.status_code == 200:
+                return
+        except:
+            pass
+
+
+waitUntilSuccess()
 display.writeText("Success!")
 subprocess.call(config.DEPLOYMENT_SCRIPT)
