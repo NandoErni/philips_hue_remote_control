0f383df4ce349baf90d8a85b4c38336c9bd21078
Nando Erni <nando.erni@outlook.de>
2021-02-02 01:52:12 +0100

    fix waitForConnect

--- src/waitForConnection.py
+++ src/waitForConnection.py
@@ -2,6 +2,7 @@ import config
 import i2cRepository
 import requests
 import subprocess
+import time
 
 display = i2cRepository.I2cRepository()
 
@@ -15,4 +16,5 @@ while not r.status_code == 200:
     except:
         pass
 
+time.sleep(30)
 subprocess.call(config.DEPLOYMENT_SCRIPT)
